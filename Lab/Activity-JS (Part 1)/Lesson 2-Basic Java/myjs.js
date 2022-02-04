e6b999a2c93633e07856a29367eeeb56b3ee0205
arief_gates <muhammadarief@gates.edu.my>
2022-02-04 11:32:01 +0800

    lab javascript

---
+++ Lab/Activity-JS (Part 1)/Lesson 2-Basic Java/myjs.js
@@ -0,0 +1,8 @@
+/* JavaScript can also be place in external file. External JavaScript file contain code to be used on several different web pages. The file extension is .js */
+
+function message()
+{
+	alert("Hello World! I'm Arief Rafie");
+	
+}
+
